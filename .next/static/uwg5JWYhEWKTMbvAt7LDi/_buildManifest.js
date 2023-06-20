e17799ff4108df44baa64489fb9f8fe060b368b1
caiostato <zombieiron9@gmail.com>
2023-06-20 01:53:19 -0300

    removing for new next and new files

--- .next/static/uwg5JWYhEWKTMbvAt7LDi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/139-f31cd5517f71960f.js","static/chunks/pages/index-b357bc3f7ac79e17.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
